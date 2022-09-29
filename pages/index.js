9625f8434735b1a5a7b14961d00257053cc92ba8
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-09-30 01:20:38 +0530

    bg implementation globally

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-zinc-100">
+      <main className="">
         <Navbar />
         <Hero />
         <Carousal />
