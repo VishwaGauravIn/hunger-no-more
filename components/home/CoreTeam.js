acb493a72e7ee83ca401f24fadef5202379f84bc
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-09-29 20:37:32 +0530

    added available avatar to core team

--- components/home/CoreTeam.js
+++ components/home/CoreTeam.js
@@ -3,44 +3,44 @@ import React from "react";
 export default function CoreTeam() {
   const cc = [
     {
-      avatar: "",
+      avatar: "/team/sn.png",
       name: "SN Tak",
       position: "Faculty Coordinator",
     },
     {
-      avatar: "",
+      avatar: "/team/pulkit.png",
       name: "Pulkit Meena",
       position: "Coordinator",
     },
     {
-      avatar: "",
+      avatar: "/team/avinash.png",
       name: "Avinash Lakhyani",
       position: "Secretary",
     },
     {
-      avatar: "",
+      avatar: "/team/vineet.png",
       name: "Vineet Barwal",
       position: "Joint Secretary",
     },
     {
-      avatar: "",
+      avatar: "/team/madhur.png",
       name: "Madhur Bansal",
       position: "Executive Member",
     },
     {
-      avatar: "",
+      avatar: "/team/ankit.png",
       name: "Ankit Gupta",
       position: "Executive Member",
     },
     {
-      avatar: "",
-      name: "Aryan Jagarwal",
-      position: "Editor",
-    },
-    {
-      avatar: "",
+      avatar: "/team/arundhati.png",
       name: "Arundhati Sharma",
       position: "Creative",
+    },
+    {
+      avatar: "/team/aryan.png",
+      name: "Aryan Jagarwal",
+      position: "Editor",
     },
   ];
   return (
@@ -61,7 +61,7 @@ export default function CoreTeam() {
               <img
                 src={cc.avatar}
                 alt=""
-                className="rounded-full w-20 h-20 md:w-28 md:h-28"
+                className="rounded-full w-20 h-20 md:w-28 md:h-28 object-cover ring ring-zinc-200"
               />
               <p className="font-medium mt-2 text-lg opacity-75">{cc.name}</p>
               <p className="text-zinc-500">{cc.position}</p>
