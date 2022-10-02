2607fffc61d14465407c512da4db4fb18abeb8a7
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-10-02 14:58:26 +0530

    minor UX impv

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -5,9 +5,9 @@ export default function Footer() {
     <>
       <div className=" bg-linear-pink-invert bg-zinc-100">
         <div className="mx-auto container pt-20 flex flex-col items-center justify-center">
-          <div className="text-black flex flex-col md:items-center f-f-l pt-3">
-            <img src="/header_logo.png" alt="" className="h-12 lg:h-14" />
-            <div className="my-6 text-base text-color f-f-l">
+          <div className="text-black flex flex-col md:items-center f-f-l pt-3 w-full">
+            <img src="/header_logo.png" alt="" className="h-12 lg:h-14 w-8/12 sm:w-auto self-center object-contain" />
+            <div className="my-6 text-base text-color f-f-l mx-4">
               <ul className="md:flex items-center">
                 <li className=" md:mr-6 cursor-pointer pt-4 lg:py-0">About</li>
                 <li className=" md:mr-6 cursor-pointer pt-4 lg:py-0">
@@ -23,7 +23,7 @@ export default function Footer() {
                 <li className="cursor-pointer pt-4 lg:py-0">Privacy Policy</li>
               </ul>
             </div>
-            <div className="text-sm text-color mb-10 f-f-l">
+            <div className="text-sm text-color mb-10 f-f-l self-center">
               <span className="text-sm">
                 <a
                   href="https://itsvg.in"
