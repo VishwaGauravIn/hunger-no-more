ee74d1fd448ebc84727d455b29b96fccdc8ec58c
Priyanshu Maurya <mauryapriyanshu-cmpn@atharvacoe.ac.in>
2022-10-08 05:54:50 -0700

    Added default-photo feature for CoreTeam section, both for male and female.

--- components/home/CoreTeam.js
+++ components/home/CoreTeam.js
@@ -5,46 +5,57 @@ export default function CoreTeam() {
     {
       avatar: "/team/sn.png",
       name: "SN Tak",
+      gender: "male",
       position: "Faculty Coordinator",
     },
     {
       avatar: "/team/pulkit.png",
       name: "Pulkit Meena",
+      gender: "male",
       position: "Coordinator",
     },
     {
       avatar: "/team/avinash.png",
       name: "Avinash Lakhyani",
+      gender: "male",
       position: "Secretary",
     },
     {
       avatar: "/team/vineet.png",
       name: "Vineet Barwal",
+      gender: "male",
       position: "Joint Secretary",
     },
     {
       avatar: "/team/madhur.png",
       name: "Madhur Bansal",
+      gender: "male",
       position: "Executive Member",
     },
     {
       avatar: "/team/ankit.png",
       name: "Ankit Gupta",
+      gender: "male",
       position: "Executive Member",
     },
     {
       avatar: "/team/arundhati.png",
       name: "Arundhati Sharma",
+      gender: "female",
       position: "Creative",
     },
     {
       avatar: "/team/aryan.png",
       name: "Aryan Jagarwal",
+      gender: "male",
       position: "Editor",
     },
   ];
   return (
-    <div className="flex flex-col w-full items-center my-10 md:my-14 " id="team">
+    <div
+      className="flex flex-col w-full items-center my-10 md:my-14 "
+      id="team"
+    >
       <p className="text-4xl font-bold">Core Team</p>
       <p className="text-gray-500 text-xl w-full md:w-8/12 text-center my-4">
         Teamwork is the fuel that allows common people to attain uncommon
@@ -60,7 +71,9 @@ export default function CoreTeam() {
             >
               <img
                 src={cc.avatar}
-                alt=""
+                alt={
+                  cc.gender === "male" ? "/team/default-male.png" : "/team/default-female.png"
+                }
                 className="rounded-full w-20 h-20 md:w-28 md:h-28 object-cover ring ring-zinc-200"
               />
               <p className="font-medium mt-2 text-lg opacity-75">{cc.name}</p>
